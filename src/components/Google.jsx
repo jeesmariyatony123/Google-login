fdb729e2486d052b2db02e7e1ec624f632ebc77f
jeesmariyatony123 <jeestony123@gmail.com>
2024-04-07 22:07:17 +0530

    google login v2

--- src/components/Google.jsx
+++ src/components/Google.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Google() {
     return (
@@ -10,12 +12,14 @@ function Google() {
                 onSuccess={credentialResponse => {
                     const decoded = jwtDecode(credentialResponse.credential);
                     console.log(decoded);
+                    toast.success(`${decoded.name} Succesfully logged in...`)
                 }}
                 onError={() => {
                     console.log('Login Failed');
                 }}
-            />
 
+            />
+            <ToastContainer position='top-center' theme='colored' autoClose={3000} />
         </div>
     )
 }
